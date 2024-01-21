728156667bb8c4f5626826eb5f0bd05802c9104d
Mohammad Zakir Hossain <engineer.zakir.hossain@gmail.com>
2024-01-21 08:44:39 +0200

    firebase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
 import './App.css';
+import initializeAuthentication from './Firebase/firebase.initialize';
+import { useState } from 'react';
+
+
+initializeAuthentication();
+
+const provider = new GoogleAuthProvider();
 
 function App() {
+  const [user, setUser] = useState({});
+
+  const handleGoogleSignIn = () => {
+    const auth = getAuth();
+    signInWithPopup(auth, provider)
+      .then(result => {
+        const {displayName, email, photoURL} = result.user;
+        
+        const logedInUser = {
+          name: displayName,
+          email: email,
+          image: photoURL
+        }
+        setUser(logedInUser);
+      })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleGoogleSignIn}>Google Sign In</button>
+      {
+        user.email && <div>
+        <h2>Welcome !!! {user.name}</h2>
+        <h3>email address: {user.email}</h3>
+        <img src={user.image}></img>
+      </div>
+      }
     </div>
   );
 }
